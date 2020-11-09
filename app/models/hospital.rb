c4b8b36058e6062422e3e4471320eda13c84215b
Jake Heft <jakeheft@gmail.com>
2020-11-09 10:21:49 -0700

    Add unique universities method

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -4,4 +4,8 @@ class Hospital < ApplicationRecord
   def doc_count
     Doctor.where('hospital_id = ?', id).count
   end
+
+  def unique_universities
+    Doctor.where('hospital_id = ?', id).distinct.pluck('doctors.university')
+  end
 end
